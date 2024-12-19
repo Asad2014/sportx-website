92e50bda10c618238e87dfc230da2e7b26680f09
asad2014 <asadkk2014@gmail.com>
2024-12-19 16:14:44 +0500

    third commit

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="px-6 py-12 text-center bg-blue-500  h-screen">
+    <div className="px-6 py-12 text-center mb-32">
       <h1 className="text-4xl font-bold mb-6">About SportX</h1>
       <p className="text-lg md:text-xl text-black mb-6 leading-relaxed text-left ">
         SportX is your ultimate destination for high-quality sports gear, apparel, and accessories. Designed for athletes and sports enthusiasts alike, SportX offers a dynamic range of products that blend performance, style, and innovation. Whether you are a professional athlete or someone embracing an active lifestyle, SportX empowers you to push boundaries and achieve greatness.
