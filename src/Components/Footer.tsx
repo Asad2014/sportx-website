6be65a0b0410455a2b36c3bc5ece428f4238a903
asad2014 <asadkk2014@gmail.com>
2024-12-19 17:19:04 +0500

    new commit

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+// import React from 'react'
+
+// const Footer = () => {
+//   return (
+//     <footer className='flex justify-center items-center bg-blue-500 h-16'>
+//       <p>&copy; 2024 SportX. All Right Reserved</p>
+//       </footer>
+//   )
+// }
+
+// export default Footer
+
+
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='flex justify-center items-center bg-blue-500 h-16'>
-      <p>&copy; 2024 SportX. All Right Reserved</p>
-      </footer>
-  )
-}
+    <footer className="flex flex-col md:flex-row justify-center items-center bg-blue-500 text-black px-6 py-4 md:h-16">
+      <p className="text-sm md:text-base text-center">
+        &copy; 2024 SportX. All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
